e77f8ee479efcc6e81a2d0a4f6d6cb39ff2f42a8
Vayne-Lover <406378362@qq.com>
2016-08-26 15:12:15 +0800

    Fix bugs and add more details.

--- Titanic/Titanic.py
+++ Titanic/Titanic.py
@@ -142,3 +142,46 @@ plot_data=pd.DataFrame({'Name':['SVM','KNN','RDM','Tuned RDM'],
                        'Test Score':[0.404,0.574,0.730,0.760]})
 sns.pointplot(x='Name',y='Train Score',data=plot_data,markers='o',color='r')
 sns.pointplot(x='Name',y='Test Score',data=plot_data,markers='D',color='g')
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.learning_curve import learning_curve
+
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+                                                            estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+    
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+                     plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                                      test_scores_mean + test_scores_std, alpha=0.1, color="g")
+                     plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+                              label="Training score")
+                     plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+                              label="Cross-validation score")
+                     
+                     plt.legend(loc="best")
+                     return plt
+
+title = "Learning Curves (Random Forest)"
+
+plot_learning_curve(clf, title, X_train, y_train, (0.7, 1.01), cv=cv_sets, n_jobs=2)
+
+plt.show()
+
+
