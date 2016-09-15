4e4884854697ba2682ba71cc11eee95f45973b24
Vayne-Lover <406378362@qq.com>
2016-09-15 11:18:36 +0800

    Change parameters.

--- House Prices/Vayne.py
+++ House Prices/Vayne.py
@@ -26,12 +26,12 @@ NQuantX = pd.DataFrame(scaledQuantX)
 scaledQuantTest = preprocessing.scale(quantTest)
 NQuantTest = pd.DataFrame(scaledQuantTest)
 
-knn =  KNeighborsRegressor(n_neighbors = 4)
+knn =  KNeighborsRegressor(n_neighbors = 5,algorithm='brute')
 knn.fit(NQuantX, y)
 
 results = knn.predict(NQuantTest)
 output['SalePrice'] = results
-output.to_csv('Vayne.csv', index=False)
+output.to_csv('Vayne3.csv', index=False)
 end=time.clock()
 
 print "Cost {:.4f}s.".format(end-start)
