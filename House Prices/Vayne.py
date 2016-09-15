967a31f302cf34786055d2707b21374fda2ccabf
Vayne-Lover <406378362@qq.com>
2016-09-15 10:38:20 +0800

    Add the solution to House Prices.

---
+++ House Prices/Vayne.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import time
+from sklearn import preprocessing
+from sklearn.neighbors import KNeighborsRegressor
+
+start=time.clock()
+train = pd.read_csv("train.csv")
+output = pd.read_csv("sample_submission.csv")
+test = pd.read_csv("test.csv")
+
+output.index = range(len(output))
+
+y = train['SalePrice']
+X = train.drop(['SalePrice'], axis=1)
+
+quantX = X._get_numeric_data()
+quantTest = test._get_numeric_data()
+
+quantX = quantX.fillna(0)
+quantTest = quantTest.fillna(0)
+
+scaledQuantX = preprocessing.scale(quantX)
+NQuantX = pd.DataFrame(scaledQuantX)
+
+scaledQuantTest = preprocessing.scale(quantTest)
+NQuantTest = pd.DataFrame(scaledQuantTest)
+
+knn =  KNeighborsRegressor(n_neighbors = 4)
+knn.fit(NQuantX, y)
+
+results = knn.predict(NQuantTest)
+output['SalePrice'] = results
+output.to_csv('Vayne.csv', index=False)
+end=time.clock()
+
+print "Cost {:.4f}s.".format(end-start)
